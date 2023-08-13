7b620c8a96f8fb0d2b85b298b9f372133621cc15
menghui-zhou <102856485+menghui-zhou@users.noreply.github.com>
2023-08-13 21:31:54 +0800

    Update update_W.m

--- update_W.m
+++ update_W.m
@@ -14,11 +14,6 @@ end
 % initialise options.
 opts = init_opts(opts);
 
-% X = multi_transpose(X);
-for tt = 1 : length(X)
-    X{tt} = X{tt}';
-end
-
 task_num  = length (X);
 dimension = size(X{1}, 2);
 funcVal = [];
